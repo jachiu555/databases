684a3c86ca868fc03c223dbb213b6c62a5585f26
jachiu555 <jachiu555@gmail.com>
2019-09-25 19:30:42 -0700

    Created a connection for our database and tested with npm start.

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,24 @@
 var mysql = require('mysql');
 
+var connection = mysql.createConnection({
+  host : 'localhost',
+  user : 'student',
+  password : 'student',
+  database : 'chat'
+});
+
+connection.connect();
+
+connection.query('select * from messages',(error,result,fields)=>{//
+  if (error)
+    throw error;
+  console.log(result);
+});
+
+connection.end();
+
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
-
+module.exports.connection = connection;//
