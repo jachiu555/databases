52f71fa5b3da08a5943ccc079678ea59ea063b98
jachiu555 <jachiu555@gmail.com>
2019-09-26 13:54:16 -0700

    Started work on GET request.

--- server/db/index.js
+++ server/db/index.js
@@ -1,24 +1,27 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-  host : 'localhost',
-  user : 'student',
-  password : 'student',
-  database : 'chat'
-});
+module.exports.con=(callback) =>{
+  var connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'student',
+    password : 'student',
+    database : 'chat'
+  });
 
-connection.connect();
+  connection.connect();
 
-connection.query('select * from messages',(error,result,fields)=>{//
-  if (error)
-    throw error;
-  console.log(result);
-});
+  connection.query('select * from messages',(error,result,fields)=>{//
+    if (error) throw error;
+    callback(result);
+  });
 
-connection.end();
+  connection.end();
+
+  //why we write like this is because
+  //you don't want to left the connection not ended before the program is closed
+  //so we write it to a whole function with a callback
+};
 
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
-
-module.exports.connection = connection;//
--- server/models/index.js
+++ server/models/index.js
@@ -1,8 +1,22 @@
 var db = require('../db');
 
+// db.con((result)=>{console.log(result[0].id)});
+var result;
+      db.con((res) => {
+        result = res;
+      console.log(result);
+      });
+
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
+    get: function () {
+      var result;
+      db.con((res) => {
+        result = res;
+      });
+      console.log(result);
+      return result;
+    }, // a function which produces all the messages
     post: function () {} // a function which can be used to insert a message into the database
   },
 
