1da3eda6dd93dc50e9ea2e014198e72fed11e21c
jachiu555 <jachiu555@gmail.com>
2019-09-25 19:31:26 -0700

    Wrote a table with MySQL and tested it. Works when running mysql.

--- server/schema.sql
+++ server/schema.sql
@@ -4,14 +4,23 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INTEGER PRIMARY KEY,
+  username TEXT NOT NULL,
+  content TEXT NOT NULL
 );
 
 /* Create other tables and define schemas for them here! */
 
+-- CREATE TABLE  (
+--   /* Describe your table here.*/
+--   id INTEGER PRIMARY KEY,
+--   username TEXT NOT NULL,
+--   content TEXT NOT NULL
+-- );
 
 
 
 /*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
+ *    mysql -u student < server/schema.sql
  *  to create the database and the tables.*/
 
